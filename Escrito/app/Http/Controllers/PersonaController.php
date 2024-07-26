3fa3d5a60e42617e00a8d0d38b6c71dc4368481f
Darked2501 <marting2007@hotmail.com>
2024-07-26 12:30:04 -0300

    Se añadio el metodo de AltaPersona y la ruta

---
+++ Escrito/app/Http/Controllers/PersonaController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Persona;
+use Illuminate\Http\Request;
+
+class PersonaController extends Controller
+{
+    public function AltaPersona(Request $request){
+        $persona = new Persona();
+        $persona->nombre = $request->nombre;
+        $persona->apellido = $request->apellido;
+        $persona->telefono = $request->telefono;
+        $persona->save();
+    }
+}
